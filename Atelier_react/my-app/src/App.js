64cae4ac8a31fca37e4bda871139263a16ce7bf4
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-03-12 22:44:51 +0100

    ajout atelier react

---
+++ Atelier_react/my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Content from "./Content/Content";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Atelier_react/my-app/src/Content/Content.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Content.css";
+
+function Content() {
+  return <content>content</content>;
+}
+
+export default Content;
